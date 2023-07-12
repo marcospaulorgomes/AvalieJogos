621dbc7564ee693e76dd7bad6870934e2d2219fe
marcospaulorgomes <marcospaulorgomes.dev@gmail.com>
2023-07-12 15:32:19 -0300

    marcos.gomes: Adição de TryCatch nas funções de apisConsumer.js

--- src/functions/apisConsumer.js
+++ src/functions/apisConsumer.js
@@ -4,16 +4,16 @@ const client = 'aybfike88n8yqkudlkht30y2tjn8ks';
 const token = 'rbrsebp3tp41al266j0abkezp6194y';
 
 
-async function igdbHelper(data){
+async function igdbHelper(data) {
 
-    for (let result = 0; result < data.length; result++){
+    for (let result = 0; result < data.length; result++) {
 
         data[result].cover = await igdbCover(data[result].cover);
 
         for (let item = 0; item < data[result].platforms.length; item++) {
             data[result].platforms[item] = await igdbPlataforms(data[result].platforms[item]);
         }
-    
+
         for (let item = 0; item < data[result].genres.length; item++) {
             data[result].genres[item] = await igdbGenres(data[result].genres[item]);
         }
@@ -22,72 +22,96 @@ async function igdbHelper(data){
 }
 
 async function igdbCover(coverId) {
-    const response = await igdb(client, token)
-        .fields(['url'])
-        .limit(1)
-        .where(`id = ${coverId}`)
-        .request('/covers');
+    try {
+        const response = await igdb(client, token)
+            .fields(['url'])
+            .limit(1)
+            .where(`id = ${coverId}`)
+            .request('/covers');
 
-    return response.data[0].url;
+        return response.data[0].url;
+    } catch (err) {
+        return err.message;
+    }
 }
 
 async function igdbPlataforms(platformsId) {
-    const response = await igdb(client, token)
-        .fields(['name'])
-        .limit(1)
-        .where(`id = ${platformsId}`)
-        .request('/platforms');
+    try {
+        const response = await igdb(client, token)
+            .fields(['name'])
+            .limit(1)
+            .where(`id = ${platformsId}`)
+            .request('/platforms');
 
-    return response.data[0].name;
+        return response.data[0].name;
+    } catch (err) {
+        return err.message;
+    }
 }
 
 async function igdbGenres(genreId) {
-    const response = await igdb(client, token)
-        .fields(['name'])
-        .limit(1)
-        .where(`id = ${genreId}`)
-        .request('/genres');
+    try {
+        const response = await igdb(client, token)
+            .fields(['name'])
+            .limit(1)
+            .where(`id = ${genreId}`)
+            .request('/genres');
 
-    return response.data[0].name;
+        return response.data[0].name;
+    } catch (err) {
+        return err.message;
+    }
 }
 
 async function igdbConsumerSearch(gameName) {
-    const response = await igdb(client, token)
-        .fields(['cover', 'name', 'total_rating', 'genres', 'platforms', 'first_release_date', 'summary'])
-        .limit(12)
-        .search(gameName)
-        .request('/games');
+    try {
+        const response = await igdb(client, token)
+            .fields(['cover', 'name', 'total_rating', 'genres', 'platforms', 'first_release_date', 'summary'])
+            .limit(12)
+            .search(gameName)
+            .request('/games');
 
         response.data = await igdbHelper(response.data);
 
-    return response.data;
+        return response.data;
+    } catch (err) {
+        return err.message;
+    }
 }
 
 async function igdbConsumerHighlights() {
-    const response = await igdb(client, token)
-        .fields(['cover', 'name', 'total_rating', 'genres', 'platforms', 'first_release_date', 'summary'])
-        .limit(7)
-        .offset(5)
-        .sort('name')
-        .where(`total_rating > ${90}`)
-        .request('/games');
+    try {
+        const response = await igdb(client, token)
+            .fields(['cover', 'name', 'total_rating', 'genres', 'platforms', 'first_release_date', 'summary'])
+            .limit(7)
+            .offset(5)
+            .sort('name')
+            .where(`total_rating > ${90}`)
+            .request('/games');
 
         response.data = await igdbHelper(response.data);
 
-    return response.data;
+        return response.data;
+    } catch (err) {
+        return err.message;
+    }
 }
 
 async function igdbConsumerGenre(genre) {
-    const response = await igdb(client, token)
-        .fields(['cover', 'name', 'total_rating', 'genres', 'platforms', 'first_release_date', 'summary'])
-        .limit(500)
-        .sort('name')
-        .where(`genres = ${genre}`)
-        .request('/games');
+    try {
+        const response = await igdb(client, token)
+            .fields(['cover', 'name', 'total_rating', 'genres', 'platforms', 'first_release_date', 'summary'])
+            .limit(500)
+            .sort('name')
+            .where(`genres = ${genre}`)
+            .request('/games');
 
         //response.data = await igdbHelper(response.data);
 
-    return response.data;
+        return response.data;
+    } catch (err) {
+        return err.message;
+    }
 }
 
 module.exports = { igdbConsumerGenre, igdbConsumerHighlights, igdbConsumerSearch };
